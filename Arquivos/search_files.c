551c701c7e983c9196b3da79ad478ca1fb55cad0
Igor Sales <igorsales.fs@gmail.com>
2019-11-15 00:37:46 -0300

    testes de status de chamados

--- Arquivos/search_files.c
+++ Arquivos/search_files.c
@@ -5,47 +5,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdio_ext.h>
+#include <string.h>
 
 #define BUFFER_SIZE 1000
+#define MAXCHAR 100
+
+int callNumbers(){
+
+  FILE *file_calls;
+  char txt[MAXCHAR];
+  int qtd;
+
+  file_calls = fopen("search_files.txt", "r");
+
+  if (file_calls){
+
+    while (fscanf(file_calls, " %1023s", txt) != EOF)
+    {
+
+      if(strcmp("qtd_de_chamados:", txt) == 0 ){                      //verifica que leu a palavra Quantidade de chamados
+           fscanf(file_calls, " %1023s", txt);                  //le a proxima coisa, que deve ser um numero
+           qtd = atoi(txt);                              //passa pra inteiro
+           //printf("Quantidade de chamados: %d\n", qtd);                              //printa ele
+      }
+    }
+    fclose(file_calls);
+  }
+  return qtd;
+}
 
 
-int main()
-{
-    /* File pointer to hold reference of input file */
-    FILE * fPtr;
-    FILE * fTemp;
+int main(){
+
+    FILE * file_calls;
+    FILE * file_temp;
     char path[100];
 
     char buffer[BUFFER_SIZE];
-    char newline[BUFFER_SIZE];
-    int line, count;
+    char newline[BUFFER_SIZE] = "qtd_de_chamados: 0\0";
+    int count, n, line = 8;
+
+    newline[19] = callNumbers() + 1;
 
 
-
-
-    printf("Enter line number to replace: ");
-    scanf("%d", &line);
 
     /* Remove extra new line character from stdin */
     //fpurge(stdin);
     setbuf(stdin, NULL);
 
 
-    printf("Replace '%d' line with: ", line);
-    fgets(newline, BUFFER_SIZE, stdin);
+  //  printf("Replace '%d' line with: ", line);
+  //  fgets(newline, BUFFER_SIZE, stdin);
 
 
     /*  Open all required files */
-    fPtr  = fopen("test_storage.txt", "r");
-    fTemp = fopen("replace_temp.txt", "w");
+    file_calls  = fopen("test_storage.txt", "r");
+    file_temp = fopen("replace_temp.txt", "w");
 
     /* fopen() return NULL if unable to open file in given mode. */
-    if (fPtr == NULL || fTemp == NULL)
+    if (file_calls == NULL || file_temp == NULL)
     {
         /* Unable to open file hence exit */
-        printf("\nUnable to open file.\n");
+        printf("\nFile couldn't be opened\n");
         printf("Please check whether file exists and you have read/write privilege.\n");
-        exit(EXIT_SUCCESS);
+        return 0;
     }
 
 
@@ -54,21 +77,21 @@ int main()
      * file after replacing given line.
      */
     count = 0;
-    while ((fgets(buffer, BUFFER_SIZE, fPtr)) != NULL)
+    while ((fgets(buffer, BUFFER_SIZE, file_calls)) != NULL)
     {
         count++;
 
         /* If current line is line to replace */
         if (count == line)
-            fputs(newline, fTemp);
+            fputs(newline, file_temp);
         else
-            fputs(buffer, fTemp);
+            fputs(buffer, file_temp);
     }
 
 
     /* Close all files to release resource */
-    fclose(fPtr);
-    fclose(fTemp);
+    fclose(file_calls);
+    fclose(file_temp);
 
 
     /* Delete original source file */
--- Arquivos/teste.c
+++ Arquivos/teste.c
@@ -8,7 +8,6 @@ int main(void)
 {
   FILE *input;
   char txt[MAXCHAR];
-  int count_line, count_line_id;
 
   input = fopen("search_files.txt", "r");
   if (input)
@@ -16,12 +15,10 @@ int main(void)
 
     while (fscanf(input, " %1023s", txt) != EOF)
     {
-      while (fscanf(input, " %1023s", txt) != "\n")
-      if(strcmp("ID:", txt) == 0 ){                      //verifica que leu a palavra ID
-           count_line_id = count_line + 1;
-           fscanf(input, " %s", txt );                  //le a proxima coisa, que deve ser um numero
+      if(strcmp("Quantidade de chamados: ", txt) == 0 ){                      //verifica que leu a palavra ID
+           fscanf(input, " %1023s", txt );                  //le a proxima coisa, que deve ser um numero
            int id = atoi(txt);                              //passa pra inteiro
-           printf("ID: %d - linha: %d\n", id, count_line_id);                              //printa ele
+           printf("%d\n", id);                              //printa ele
       }
     }
     fclose(input);
